bd5dca1bbe67e6cba0af4a49ca1da304f0cb1035
David Graham <david.graham@aize.io>
2024-03-05 03:37:33 +0100

    chore: add build to gitignore

--- build/service-worker-loader.js
+++ build/service-worker-loader.js
@@ -1,1 +1,3 @@
-import './assets/service-worker.ts-VSDpFfad.js';
+import 'http://localhost:5173/@vite/env';
+import 'http://localhost:5173/@crx/client-worker';
+import 'http://localhost:5173/service-worker.ts';
