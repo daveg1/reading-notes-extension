b658ddfe66f050df0a5a56045933913f0f1ef766
David Graham <david.graham@aize.io>
2024-03-06 22:39:49 +0100

    feat: add contextmenu item

--- service-worker.ts
+++ service-worker.ts
@@ -1,3 +1,40 @@
+// import { createTextFragment } from './utils'
+
 chrome.sidePanel
   .setPanelBehavior({ openPanelOnActionClick: true })
   .catch(console.error)
+
+chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+  if (!tab || !info.selectionText) return
+
+  const results = await chrome.scripting.executeScript({
+    target: { tabId: tab.id! },
+    func: async (selection: string) => {
+      // await createTextFragment(selection)
+    },
+    args: [info.selectionText],
+  })
+
+  const textFragment = results[0].result
+  console.log(info)
+  // console.log(textFragment)
+})
+
+// Add new context menu item
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create(
+    {
+      id: 'selection',
+      title: 'Save as reading note',
+      contexts: ['selection'],
+    },
+    () => {
+      if (chrome.runtime.lastError) {
+        console.error(
+          'Failed to create contextmenu item: ',
+          chrome.runtime.lastError?.message
+        )
+      }
+    }
+  )
+})
